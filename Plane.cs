b4d0821dec1bb165708497c179f5d6ee77e2e3cc
GAO-ZHEN <k080058496@gmail.com>
2017-05-16 22:40:03 +0800

    20170516

---
+++ Plane.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plane : MonoBehaviour {
+	public bool showpath=true;
+	public Color pathColor=Color.red;
+	public bool loop=true;
+	public float Radius=2.0f;
+	public Transform[] wayPoint;
+	// Use this for initialization
+	void Reset(){
+		wayPoint = new Transform[GameObject.FindGameObjectsWithTag ("wayPoint").Length];
+		
+		for (int cnt=0; cnt<wayPoint.Length; cnt++) {
+			wayPoint[cnt]=GameObject.Find("wayPoint_"+(cnt+1).ToString()).transform;
+		}
+	}
+	public float Length{
+		get{
+			return wayPoint.Length;
+		}
+	}
+	public Vector3 GetPosition(int index){
+		return wayPoint [index].position;
+	}
+	void OnDrawGizmos(){
+		if (!showpath)return;
+
+		for (int i=0; i<wayPoint.Length; i++) {
+			if(i+1<wayPoint.Length){
+				Debug.DrawLine(wayPoint[i].position,wayPoint[i+1].position,pathColor);
+			}
+			else{
+				if(loop){
+					Debug.DrawLine(wayPoint[i].position,wayPoint[0].position,pathColor);
+				}
+			}
+		}
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
---
+++ robot.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class robot : MonoBehaviour {
+	public Plane path;
+	public float speed=20.0f;
+	public float mass=5.0f;
+	public bool looping=true;
+
+	private float botSpeed;
+	private int PathIndex;
+	private float PathLength;
+	private Vector3 targetPosition;
+	private Vector3 botVelocity;
+	public Vector3 Accelerate(Vector3 target){
+		Vector3 desiredVelocty = target - transform.position;
+		
+		desiredVelocty.Normalize ();
+		desiredVelocty *= botSpeed;
+		
+		Vector3 steeringForce = desiredVelocty - botVelocity;
+		Vector3 acceleration = steeringForce / mass;
+		return acceleration;
+	}
+	// Use this for initialization
+	void Start () {
+		PathLength = path.Length;
+		PathIndex = 0;
+		botVelocity = transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		botSpeed = speed * Time.deltaTime;
+		targetPosition = path.GetPosition (PathIndex);
+
+		if (Vector3.Distance (transform.position, targetPosition) < path.Radius) {
+			if(PathIndex < PathLength-1)
+				PathIndex++;
+			else if(looping)
+				PathIndex=0;
+			else return;
+
+
+        }
+		botVelocity += Accelerate (targetPosition);
+		transform.position += botVelocity;
+		transform.rotation = Quaternion.LookRotation (botVelocity);
+	}
+
+}
