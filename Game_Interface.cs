a9ef929a5b6d6fe5c269e72109179e47e6546e4b
Lin GAO-ZHEN <k080058496@gmail.com>
2017-06-19 17:12:41 +0800

    Add files via upload

---
+++ Game_Interface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game_Interface : MonoBehaviour {
+    public Button goStart;
+    public Button GameQuit;
+    void Start()
+    {
+        Button inx1 = goStart.GetComponent<Button>();
+        inx1.onClick.AddListener(GameStart);
+        Button inx2 = GameQuit.GetComponent<Button>();
+        inx2.onClick.AddListener(QuitGame);
+    }
+    void GameStart()
+    {
+        Application.LoadLevel(1);
+    }
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Target.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour {
+    public bool Hit_1;
+    public bool Hit_2;
+    public bool Hit_3;
+    public bool Hit_4;
+    public int point;
+    
+    // Use this for initialization
+    void Start () {
+       
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Hit_1 == true)
+        {
+            point += 10;
+            Hit_1 = false;
+        }
+        if (Hit_2 == true)
+        {
+            point += 9;
+            Hit_2 = false;
+        }
+        if (Hit_3 == true)
+        {
+            point += 8;
+            Hit_3 = false;
+        }
+        if (Hit_4 == true)
+        {
+            point += 7;
+            Hit_4 = false;
+        }
+        
+    }
+}
